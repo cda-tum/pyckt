092ecbf837f4c3b0556d828d6dbb2f57bd1f7391
Phuoc Pham <phuocphn@gmail.com>
2025-12-11 09:05:21 +0100

    :sparkles: add HL3/sb and HL3/tc

---
+++ src/topogen/HL3/sb.py
@@ -0,0 +1,111 @@
+from src.topogen.HL3.lp import *
+from src.topogen.HL2.vb import *
+from src.topogen.common.circuit import *
+
+
+from pathlib import Path
+from typing import Callable, Iterator
+from itertools import chain
+
+
+# fmt: off
+
+GALLERY_DOT_DIR = Path(__file__).parent.parent.parent.parent / "gallery" / "HL3" / "sb" / "dots"
+GALLERY_DOT_DIR.mkdir(parents=True, exist_ok=True)
+
+GALLERY_IMAGE_DIR = Path(__file__).parent.parent.parent.parent / "gallery" / "HL3" / "sb" / "images"
+GALLERY_IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+
+def connectInstanceTerminalsOfOneTransistorStageBias(stageBias:Circuit, currentBias:Circuit) -> Circuit:
+    connect((stageBias, "in"), (currentBias, "in"))
+    connect((stageBias, "out"), (currentBias, "out"))
+    connect((stageBias, "source"), (currentBias, "source"))
+    return stageBias
+
+def connectInstanceTerminalsOfTwoTransistorStageBias(stageBias:Circuit, currentBias:Circuit) -> Circuit:
+    connect((stageBias, "inoutput"), (currentBias, "inoutput"))
+    connect((stageBias, "insource"), (currentBias, "insource"))
+    connect((stageBias, "inner"), (currentBias, "inner"))
+    connect((stageBias, "out"), (currentBias, "out"))
+    connect((stageBias, "source"), (currentBias, "source"))
+    return stageBias
+
+def createOneTransistorStageBias(currentBias) -> Circuit:
+    sb = StageBias(id=1, techtype="?")
+    sb.ports = [
+        "out",
+        "in",
+        "source",
+    ]
+    sb.add_instance(currentBias)
+    sb = connectInstanceTerminalsOfOneTransistorStageBias(sb, currentBias)
+    return sb
+
+def createTwoTransistorStageBias(currentBias) -> Circuit:
+    sb = StageBias(id=1, techtype="?")
+    sb.ports = [
+        "out",
+        "inoutput",
+        "insource",
+        "inner",
+        "source",
+    ]
+    sb.add_instance(currentBias)
+    sb = connectInstanceTerminalsOfTwoTransistorStageBias(sb, currentBias)
+    return sb
+
+def createOneTransistorStageBiases(oneTransistorCurrentBiases: list[Circuit])->Iterator[Circuit]:
+    for currentBias in oneTransistorCurrentBiases:
+        yield createOneTransistorStageBias(currentBias)
+
+def createTwoTransistorStageBiases(twoTransistorCurrentBiases) ->Iterator[Circuit]:
+    for currentBias in twoTransistorCurrentBiases:
+        yield createTwoTransistorStageBias(currentBias)
+
+def initializeStageBiasesPmos():
+    oneTransistorCurrentBiases = CurrentBiasManager().getOneTransistorCurrentBiasesPmos()
+    twoTransistorCurrentBiases = CurrentBiasManager().getTwoTransistorCurrentBiasesPmos()
+    return chain(createOneTransistorStageBiases(oneTransistorCurrentBiases), createTwoTransistorStageBiases(twoTransistorCurrentBiases))
+
+def initializeStageBiasesNmos():
+    oneTransistorCurrentBiases = CurrentBiasManager().getOneTransistorCurrentBiasesNmos()
+    twoTransistorCurrentBiases = CurrentBiasManager().getTwoTransistorCurrentBiasesNmos()
+    return chain(createOneTransistorStageBiases(oneTransistorCurrentBiases), createTwoTransistorStageBiases(twoTransistorCurrentBiases))
+
+
+class StageBiasManager:
+    def __init__(self):
+        pass
+    def createStageBiasesPmos(self) ->Iterator[Circuit]:
+        return initializeStageBiasesPmos()
+    def createStageBiasesNmos(self) ->Iterator[Circuit]:
+        return initializeStageBiasesNmos()
+    
+    def getOneTransistorStageBiasesNmos(self) ->Iterator[Circuit]:
+        oneTransistorCurrentBiases = CurrentBiasManager().getOneTransistorCurrentBiasesNmos()
+        return createOneTransistorStageBiases(oneTransistorCurrentBiases)
+    def getTwoTransistorStageBiasesNmos(self) ->Iterator[Circuit]:
+        twoTransistorCurrentBiases = CurrentBiasManager().getTwoTransistorCurrentBiasesNmos()
+        return createTwoTransistorStageBiases(twoTransistorCurrentBiases)
+    
+
+
+if __name__ == "__main__":
+    mng = StageBiasManager()
+    methods: list[Callable[[], Iterator[Circuit]]] = [
+        mng.createStageBiasesPmos,
+        mng.createStageBiasesNmos,
+    ]
+
+    for case_id, create_method in enumerate(methods, start=1):
+        circuits = list(create_method())
+        print(f"case {case_id}: {create_method.__name__}, len = {len(circuits)}")
+        for circuit_id, load in enumerate(circuits, start=1):
+            save_graphviz_figure(
+                load,
+                GALLERY_DOT_DIR / f"l_{case_id}_{circuit_id}.dot",
+            )
+            convert_dot_to_png(
+                GALLERY_DOT_DIR / f"l_{case_id}_{circuit_id}.dot",
+                GALLERY_IMAGE_DIR / f"l_{case_id}_{circuit_id}.png",
+            )
---
+++ src/topogen/HL3/tc.py
@@ -0,0 +1,147 @@
+from src.topogen.HL3.lp import *
+from src.topogen.HL2.vb import *
+from src.topogen.HL2.dp import *
+
+from src.topogen.common.circuit import *
+
+
+from pathlib import Path
+from typing import Callable, Iterator
+from itertools import chain
+from copy import deepcopy
+
+# fmt: off
+
+GALLERY_DOT_DIR = Path(__file__).parent.parent.parent.parent / "gallery" / "HL3" / "tc" / "dots"
+GALLERY_DOT_DIR.mkdir(parents=True, exist_ok=True)
+
+GALLERY_IMAGE_DIR = Path(__file__).parent.parent.parent.parent / "gallery" / "HL3" / "tc" / "images"
+GALLERY_IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def connectInstanceTerminalsOfSimpleTransconductance(tc: Circuit, differentialPair: Circuit) -> Circuit:
+    connect((tc, "input1"), (differentialPair, "input1"))
+    connect((tc, "input2"), (differentialPair, "input2"))
+    connect((tc, "out1"), (differentialPair, "output1"))
+    connect((tc, "out2"), (differentialPair, "output2"))
+    connect((tc, "source"), (differentialPair, "source"))
+    return tc
+
+def connectInstanceTerminalsOfFeedbackTransconductance(tc: Circuit, differentialPair1: Circuit, differentialPair2: Circuit) -> Circuit:
+    connect((tc, "input1"), (differentialPair1, "input1"))
+    connect((tc, "inner"), (differentialPair1, "input2"))
+    connect((tc, "out1"), (differentialPair1, "output1"))
+    connect((tc, "out2"), (differentialPair1, "output2"))
+    connect((tc, "source1"), (differentialPair1, "source"))
+
+
+    connect((tc, "input2"), (differentialPair2, "input1"))
+    connect((tc, "inner"), (differentialPair2, "input2"))
+    connect((tc, "out1"), (differentialPair2, "output1"))
+    connect((tc, "out2"), (differentialPair2, "output2"))
+    connect((tc, "source2"), (differentialPair2, "source"))
+    return tc
+
+def connectInstanceTerminalsOfComplementaryTransconductance(tc: Circuit, differentialPairNmos: Circuit, differentialPairPmos: Circuit) -> Circuit:
+    connect((tc, "input1"), (differentialPairNmos, "input1"))
+    connect((tc, "input2"), (differentialPairNmos, "input2"))
+    connect((tc, "out1nmos"), (differentialPairNmos, "output1"))
+    connect((tc, "out2nmos"), (differentialPairNmos, "output2"))
+    connect((tc, "source_nmos"), (differentialPairNmos, "source"))
+
+
+    connect((tc, "input1"), (differentialPairPmos, "input1"))
+    connect((tc, "input2"), (differentialPairPmos, "input2"))
+    connect((tc, "out1pmos"), (differentialPairPmos, "output1"))
+    connect((tc, "out2pmos"), (differentialPairPmos, "output2"))
+    connect((tc, "source_pmos"), (differentialPairPmos, "source"))
+    return tc
+
+def createSimpleTransconductance(differentialPair):
+    tc = Transconductance(id=1, techtype="?")
+    tc.ports = [
+        "out1",
+        "out2",
+        "input1",
+        "input2",
+        "source",
+    ]
+    tc.add_instance(differentialPair)
+    tc = connectInstanceTerminalsOfSimpleTransconductance(tc, differentialPair)
+    return tc
+
+def createFeedbackTransconductance(differentialPair)-> Circuit:
+    differentialPair1 = deepcopy(differentialPair)
+    differentialPair2 = deepcopy(differentialPair)
+    tc = Transconductance(id=1, techtype="?")
+    tc.ports = [
+        "out1",
+        "out2",
+        "input1",
+        "input2",
+        "source1",
+        "source2",
+    ]
+    tc.add_instance(differentialPair1)
+    tc.add_instance(differentialPair2)
+
+    tc = connectInstanceTerminalsOfFeedbackTransconductance(tc, differentialPair1, differentialPair2)
+    return tc
+
+def createComplementaryTransconductance(differentialPairPmos, differentialPairNmos) -> Circuit:
+    tc = Transconductance(id=1, techtype="?")
+    tc.ports = [
+        "out1nmos",
+        "out2nmos",
+        "out1pmos",
+        "out2pmos",
+        "input1",
+        "input2",
+        "source_pmos",
+        "source_nmos",
+    ]
+    tc.add_instance(differentialPairPmos)
+    tc.add_instance(differentialPairNmos)
+    tc = connectInstanceTerminalsOfComplementaryTransconductance(tc, differentialPairNmos, differentialPairPmos)
+    return tc
+
+class TransconductanceManager:
+    def __init__(self):
+        pass
+    
+    def createSimpleTransconductance(self) -> Iterator[Circuit]:
+        differentialPairPmos = DiffPairManager().getDifferentialPairPmos()
+        differentialPairNmos = DiffPairManager().getDifferentialPairNmos()
+        return iter([createSimpleTransconductance(differentialPairPmos), createSimpleTransconductance(differentialPairNmos)])
+    
+    def createFeedbackTransconductance(self) -> Iterator[Circuit]:
+        differentialPairPmos = DiffPairManager().getDifferentialPairPmos()
+        differentialPairNmos = DiffPairManager().getDifferentialPairNmos()
+        return iter([createFeedbackTransconductance(differentialPairPmos), createFeedbackTransconductance(differentialPairNmos)])
+    
+    def createComplementaryTransconductance(self) -> Iterator[Circuit]:
+        differentialPairPmos = DiffPairManager().getDifferentialPairPmos()
+        differentialPairNmos = DiffPairManager().getDifferentialPairNmos()
+        return iter([createComplementaryTransconductance(differentialPairPmos, differentialPairNmos)])
+
+
+if __name__ == "__main__":
+    mng = TransconductanceManager()
+    methods: list[Callable[[], Iterator[Circuit]]] = [
+        mng.createSimpleTransconductance,
+        mng.createFeedbackTransconductance,
+        mng.createComplementaryTransconductance,
+    ]
+
+    for case_id, create_method in enumerate(methods, start=1):
+        circuits = list(create_method())
+        print(f"case {case_id}: {create_method.__name__}, len = {len(circuits)}")
+        for circuit_id, load in enumerate(circuits, start=1):
+            save_graphviz_figure(
+                load,
+                GALLERY_DOT_DIR / f"l_{case_id}_{circuit_id}.dot",
+            )
+            convert_dot_to_png(
+                GALLERY_DOT_DIR / f"l_{case_id}_{circuit_id}.dot",
+                GALLERY_IMAGE_DIR / f"l_{case_id}_{circuit_id}.png",
+            )
