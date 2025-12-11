37ea49d211e7bb0ca2dd2d614ee4bbfe103e749e
Phuoc Pham <phuocphn@gmail.com>
2025-12-11 09:08:29 +0100

    :wheelchair: add getter methods for DiffPairManager class

--- src/topogen/HL2/dp.py
+++ src/topogen/HL2/dp.py
@@ -3,7 +3,7 @@ from src.topogen.common.circuit import *
 
 
 dp1 = DiffPair(techtype="p", id=1)
-dp1.ports = (["input1", "input2", "output1", "output2", "source"],)
+dp1.ports = ["input1", "input2", "output1", "output2", "source"]
 dp1.add_instance(NormalTransistor(techtype="p"))
 dp1.add_instance(NormalTransistor(techtype="p"))
 dp1.add_connection_xxx(port="output1", instance_id=0, instance_port="drain")
@@ -14,7 +14,7 @@ dp1.add_connection_xxx(port="input2", instance_id=1, instance_port="gate")
 dp1.add_connection_xxx(port="source", instance_id=1, instance_port="source")
 
 dp2 = DiffPair(techtype="n", id=2)
-dp2.ports = (["input1", "input2", "output1", "output2", "source"],)
+dp2.ports = ["input1", "input2", "output1", "output2", "source"]
 dp2.add_instance(NormalTransistor(techtype="n"))
 dp2.add_instance(NormalTransistor(techtype="n"))
 dp2.add_connection_xxx(port="output1", instance_id=0, instance_port="drain")
@@ -28,3 +28,9 @@ dp2.add_connection_xxx(port="source", instance_id=1, instance_port="source")
 class DiffPairManager:
     def getAllDiffPairs(self):
         return [dp1, dp2]
+
+    def getDifferentialPairPmos(self):
+        return dp1
+
+    def getDifferentialPairNmos(self):
+        return dp2
