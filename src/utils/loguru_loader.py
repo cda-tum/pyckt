03897268c8d655ccc91c665567c00e0a8e6188d6
Phuoc Pham <phuocphn@gmail.com>
2025-12-18 11:04:06 +0100

    :sparkles: add Singleton Logger

--- src/utils/loguru_loader.py
+++ src/utils/loguru_loader.py
@@ -2,6 +2,40 @@ import sys
 from loguru import logger
 
 
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Logger(metaclass=Singleton):
+    def __init__(self, name="main"):
+        if hasattr(self, "_initialized"):
+            return
+
+        logger.remove()
+        log_level = "DEBUG"
+        log_format = (
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <8}</level>| "
+            "<cyan>{module}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+            "<level>{message}</level>"
+        )
+        logger.add(sys.stdout, format=log_format, level=log_level)
+
+        self.logger = logger
+        self._initialized = True
+
+    def debug(self, message: str):
+        self.logger.debug(message)
+
+    def info(self, message: str):
+        self.logger.info(message)
+
+
 # Logger setup
 def setup_logger(log_level="DEBUG", log_format=None, log_file=None):
     logger.remove()
